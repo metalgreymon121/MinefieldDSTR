d498f11da4506766a8d03841e9be299093bafdaa
Marco <icefireclover@gmail.com>
2016-06-16 19:37:01 -0500

    Supports landscape and portrait modes. Outlined Fragment screen aka the basketball court minefield area. Experimenting with OpenGL ES 2.0

--- app/src/main/java/com/entc489/marcomendiola/minefielddstr/MainActivity.java
+++ app/src/main/java/com/entc489/marcomendiola/minefielddstr/MainActivity.java
@@ -19,13 +19,12 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
-
+/*
+* This class will call the screen fragment,
+* query the ThingWorx cloud to get data on the pressure and temperature of the basketball court,
+* and send data to the ThingWorx cloud to start, pause or stop the DSTR robots.
+*/
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-
-    Canvas canvas;
-    Paint paint;
-    private static Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +41,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         pause.setOnClickListener(this);
         stop.setOnClickListener(this);
 
-        //draw basketball court;
-        // createRect(MainActivity.context = getApplicationContext());
+        //place screenfragment
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         ScreenFragment screenFragment = ScreenFragment.newInstance();
@@ -73,61 +71,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
-    public void createRect(Context context) {
-        float width = 0;
-        float height = 0;
-
-        Resources resources = context.getResources();
-        DisplayMetrics metrics = resources.getDisplayMetrics();
-        width = metrics.widthPixels;
-        height = metrics.heightPixels;
-
-        float left = 0;
-        float top = 0;
-        float right = width - 16;
-        float bottom = (height * 75) / 100;
-
-        paint.setColor(Color.BLACK);
-        canvas.drawRect(left, top, right, bottom, paint);
-    }
-
-    /**
-     * This method converts dp unit to equivalent pixels, depending on device density.
-     * dp  = A value in dp (density independent pixels) unit. Which we need to convert into pixels
-     * context = Context to get resources and device specific display metrics
-     * Returns: A float value to represent px equivalent to dp depending on device density
-     */
-    public static float convertDpToPixel(float dp, Context context) {
-        Resources resources = context.getResources();
-        DisplayMetrics metrics = resources.getDisplayMetrics();
-        float px = dp * ((float) metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
-        return px;
-    }
-
-    /**
-     * This method converts device specific pixels to density independent pixels.
-     * px = A value in px (pixels) unit. Which we need to convert into db
-     * context = Context to get resources and device specific display metrics
-     * Returns: A float value to represent dp equivalent to px value
-     */
-    public static float convertPixelsToDp(float px, Context context) {
-        Resources resources = context.getResources();
-        DisplayMetrics metrics = resources.getDisplayMetrics();
-        float dp = px / ((float) metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
-        return dp;
-    }
-
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonStart:
-                Toast.makeText(getApplicationContext(), "Start works", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Start works", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.buttonPause:
-                Toast.makeText(getApplicationContext(), "Pause works", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Pause works", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.buttonStop:
-                Toast.makeText(getApplicationContext(), "Stop works", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Stop works", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
--- app/src/main/java/com/entc489/marcomendiola/minefielddstr/ScreenFragment.java
+++ app/src/main/java/com/entc489/marcomendiola/minefielddstr/ScreenFragment.java
@@ -1,19 +1,32 @@
 package com.entc489.marcomendiola.minefielddstr;
 
 
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * This fragment is in charge of drawing the basketball court.
+ * It will draw the static locations of the beacons,
+ * the location of the DSTR and ASEP robots in real time,
+ * and the locations of the mines the DSTR robots find.
  */
 public class ScreenFragment extends Fragment {
+    private final int CONTEXT_CLIENT_VERSION = 2;
+    private GLSurfaceView glSurfaceView;
 
+    int amountofMinesFound = 0;
+    DSTR DSTR1 = new DSTR(0, 0, false);
+    DSTR DSTR2 = new DSTR(0, 0, false);
+    DSTR DSTR3 = new DSTR(0, 0, false);
+    DSTR DSTR4 = new DSTR(0, 0, false);
 
+    //create a new instance of this fragment
     public static ScreenFragment newInstance() {
         ScreenFragment fragment = new ScreenFragment();
         return fragment;
@@ -23,8 +36,37 @@ public class ScreenFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_screen, container, false);
+        View view = inflater.inflate(R.layout.fragment_screen, container, false);
+        glSurfaceView = (GLSurfaceView) view.findViewById(R.id.glSurfaceView);
+        glSurfaceView.setEGLContextClientVersion(3);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        //setting your renderer here causes onSurfaceCreated to be called
+        //if you set your renderer here then you have a context to load resources
+        super.onActivityCreated(savedInstanceState);
+        glSurfaceView.setEGLContextClientVersion(CONTEXT_CLIENT_VERSION);
+        glSurfaceView.setRenderer(new ScreenRenderer(getActivity()));
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("Fragment", "OnCreate");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
     }
 
 }
